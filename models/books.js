a2ce4e624a1ac9abb82f22638ed7e54c12f98889
jhf1203 <jhf1203@gmail.com>
2020-11-27 20:06:30 -0500

    found the error issue in list modal for other users, need to reserach solution

--- models/books.js
+++ models/books.js
@@ -13,7 +13,7 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false
     },
     description: {
-      type: DataTypes.TEXT
+      type: DataTypes.STRING
     },
     state: {
       type: DataTypes.STRING
